ace3a4ca538bcf79a0226d352c27ad08e40e0d8e
alinaasrzad <95015661+alinaasrzad@users.noreply.github.com>
2025-09-08 02:46:36 +0330

    new files

---
+++ script.js
@@ -0,0 +1,49 @@
+// const name = "alina";
+// let name1 = "fardan";
+
+// lesson 2
+
+// console.log(name1);
+// console.log(name);
+
+// lesson 3
+
+// object type
+// object = {}
+// let name = " alina";
+// let age = 23;
+// let admin = true;
+// let person ={
+//     name :"alina",
+//     age : 23,
+//     admin :true,
+// };
+// console.log(person);
+// console.log(person.age);
+
+//***********array type */
+// lesson 4
+
+// let users = [
+//     "alina" , "ali","lala"
+//     ]
+//     users[4] = "salam"
+//     // if you want to add object in array
+//     users[1] = " hello"
+//     // you can change your elements in array
+//     users[1] = 4
+//     // it can be a number
+//     console.log(users[4]);
+//     console.log(users);
+//   console.log(users.length)
+// //   its clear
+//   console.log(users.includes("salam"))
+
+// if the number is not in it  = undefind
+
+// type of function
+//  
+function deep(name ,age){
+    console.log("hello",name ,"that you have a " ,age)
+}
+deep("alina" , 33) 
