68d5f2099dae4df69dd6a6d91ab595213e46468c
alinaasrzad <95015661+alinaasrzad@users.noreply.github.com>
2025-09-10 00:08:26 +0330

    added files

--- script.js
+++ script.js
@@ -98,4 +98,16 @@ console.log(cantbuy)
 let f = 4 + 5 * 2;
 //* is the first
 console.log(f)
+// 6 am - 12pm -> good morning
+// 12pm -6pm -> good afternoon 
+// (otherwise)other time -> good evening
+let hour = 15;
+if (hour => 6 && hour < 12 ){
+        console.log("good morning")
+}else if (hour =>12 && hour<18){
+console.log("good afternoon")
+}else{
+    console.log("good evening")
+}
+
 
