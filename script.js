312f73d462a89332f878e43d930749846d3ac611
alinaasrzad <95015661+alinaasrzad@users.noreply.github.com>
2025-09-13 21:31:11 +0330

    added text

--- script.js
+++ script.js
@@ -109,5 +109,17 @@ console.log("good afternoon")
 }else{
     console.log("good evening")
 }
-
+/// ****                ****                ****
+let role = "guest"
+switch(role){
+        case "guest":
+        console.log("guest user");
+        break;
+            case "admin":
+        console.log("admin user");
+        break;
+        default:
+        console.log("unknown user")
+        
+}
 
