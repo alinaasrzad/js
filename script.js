6c70c2578236c251f72cf8860af11ab9ffa83c1d
alinaasrzad <95015661+alinaasrzad@users.noreply.github.com>
2025-09-15 20:10:26 +0330

    updated

--- script.js
+++ script.js
@@ -129,8 +129,12 @@ switch(role){
 // console("neon");
 // console("neon");
  
-for(let i=0;i<3;i++ ){
+for(let i=0;i<30;i++ ){
+    if(i % 2===0){
+console.log(i)
+    }
     console.log("neon");
+    
 }
 ;
 
