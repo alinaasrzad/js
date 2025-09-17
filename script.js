925958963cc89ad23d3d5bf0ae662c2704fa3337
alinaasrzad <95015661+alinaasrzad@users.noreply.github.com>
2025-09-17 20:32:28 +0330

    up

--- script.js
+++ script.js
@@ -32,6 +32,7 @@
 //     users[1] = " hello"
 //     // you can change your elements in array
 //     users[1] = 4
+
 //     // it can be a number
 //     console.log(users[4]);
 //     console.log(users);
@@ -42,54 +43,58 @@
 // if the number is not in it  = undefind
 
 // type of function
-//  
-function deep(name ,age){
-    console.log("hello"+name +"that you have a " +age)
+//
+function deep(name, age) {
+  console.log("hello" + name + "that you have a " + age);
 }
-deep("alina" , 33) 
+deep("alina", 33);
 //tamrin
-function square(number){
-    return number *number 
+function square(number) {
+  return number * number;
 }
-let  result = square(5)
-console.log(result)
+let result = square(5);
+console.log(result);
+
 //******                       operators ****                            **** */
-let x = 10;
-let y =15;
+// let x = 10;
+// let y = 15;
 // x = x+y;
 // console.log(x+y)
-// console.log(x**y) 
+// console.log(x**y)
 // console.log(x/y)
 // console.log(x++)
 // // console.log(++x)
 // // console.log(+x)
 // console.log(x<4)
+
 // console.log(y<8)
+
 // console.log(x<16)
 // console.log(x == 1)
-// console.log(10 === 10) 
+// console.log(10 === 10)
 // seta === type ham check mishe
-
 
 //*****                     ***                 *** */
 let athenticated = false;
-let bottonText =  athenticated ? "dashboard" : "login"
-console.log(bottonText)
+let bottonText = athenticated ? "dashboard" : "login";
+
+console.log(bottonText);
 
 // console.log(x%y)    baqimande
- 
+
 //logical and
 // &&
-console.log(false && false)
+console.log(false && false);
 let ProductCount = 3;
 let ProductPrice = 1000;
- let credit = 1070;
- let buy = ProductCount > 0 && credit >ProductPrice;
- console. log(buy);
+let credit = 1070;
+
+let buy = ProductCount > 0 && credit > ProductPrice;
+console.log(buy);
 // logical OR
 // ||
- let cantbuy = ProductCount > 6 || credit >ProductPrice;
-console.log(cantbuy)
+let cantbuy = ProductCount > 6 || credit > ProductPrice;
+console.log(cantbuy);
 
 //not
 
@@ -97,52 +102,51 @@ console.log(cantbuy)
 //taqadom amalgara
 let f = 4 + 5 * 2;
 //* is the first
-console.log(f)
+console.log(f);
 // 6 am - 12pm -> good morning
-// 12pm -6pm -> good afternoon 
+// 12pm -6pm -> good afternoon
 // (otherwise)other time -> good evening
 let hour = 15;
-if (hour >= 6 && hour < 12 ){
-        console.log("good morning")
-}else if (hour >= 12 && hour<18){
-console.log("good afternoon")
-}else{
-    console.log("good evening")
+if (hour >= 6 && hour < 12) {
+  console.log("good morning");
+} else if (hour >= 12 && hour < 18) {
+  console.log("good afternoon");
+} else {
+  console.log("good evening");
 }
 /// ****                ****                ****
-let role = "admin"
-switch(role){
-        case "guest":
-        console.log("guest user");
-        break;
-            case "admin":
-        console.log("admin user");
-        break;
-        default:
-        console.log("unknown user")
-        
+let role = "admin";
+switch (role) {
+  case "guest":
+    console.log("guest user");
+    break;
+  case "admin":
+    console.log("admin user");
+
+    break;
+  default:
+    console.log("unknown user");
 }
 //***               **              ***  */
 //for
 ///while
 //do while
 //  for .. in
+
 //for .. of
 // console("neon");
 // console("neon");
 // console("neon");
 // console("neon");
 // console("neon");
- 
+
 // for(let i=0;i<30;i++ ){
 //     if(i % 2===0){
 // console.log(i)
 //     }
 //     console.log("neon");
-    
-// }
-;
 
+// }
 //**        ***         ***         ***         *** */
 // let i= 0 ;
 // while(i<=8){
@@ -156,34 +160,45 @@ switch(role){
 // }while(i<5);
 
 const person = {
-    name:"alina",
-    age :27,
-    user:"admin"
-}
-const colors= ["red","blue","pink"]
+  name: "alina",
+  age: 27,
+  user: "admin",
+};
+
+const colors = ["red", "blue", "pink"];
 // key inja yani name , age , user
 for (const key in person) {
-   console.log(key),
-   console.log(person[key])
-
+  console.log(key), console.log(person[key]);
 }
-for (index in colors )
-{
-    console.log(index)
-    console.log(colors[index])
-
+for (index in colors) {
+  console.log(index);
+  console.log(colors[index]);
 }
 //index inja shomare haye har kodom ro mizare
-for (let color of colors)
-{
-    console.log(color)
+for (let color of colors) {
+  console.log(color);
 }
 let i = 0;
-while (i<10){
-    if (i===5){
-i++;
-    continue;
-    }
+while (i <= 10) {
+  if (i === 4) {
     console.log(i);
-        i++;    
+    i++;
+    continue;
+  }
+  console.log(i);
+  i++;
 }
+
+//***               ****                ****                    ****            ** */
+
+let radius = 2;
+let x = 1;
+let y = 1;
+
+let circle = {
+  radius: 2,
+  location: {
+    x: 1,
+    y: 1,
+  },
+};
